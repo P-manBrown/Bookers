caca7c147bd880c5b138530a3d92eb49fddefcd4
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-20 04:29:42 +0000

    [add] 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,14 @@
 class BooksController < ApplicationController
   def index
-  end
-  
-  def new
+    @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
+    p book
     book.save
-    redirect_to book_path
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -17,9 +16,9 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
-  
-  
+
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
